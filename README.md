5e0183c04b25e7a554ae29470015bb9d4fe26d0c
duaraghav8@gmail <duaraghav8@gmail.com>
2015-11-24 19:42:38 +0530

    Huffman Encoding - Tree Construction

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Data-Compression
 Explanation, Analysis and Implementation of Data Compression Algorithms
+
+1. Huffman Encoding Compression
---
+++ huffman_coding/python/huffman_encoding.py
@@ -0,0 +1,57 @@
+#################################################################################################################################
+#Huffman Encoding (HC) Algorithm through HC Tree
+#Create a HTree Object and call construct () with 1 argument - a dictionary in which key=>character, value=>Frequency of character.
+#call get_encodings () on the object to get a dictionary in which key=>character, value=>Huffman Code.
+#returns empty dict if construct () was never called or construct () was supplied with empty dict
+#Time Complexity: O (Nlog (N))
+#################################################################################################################################
+
+from heapq import heapify, heappush, heappop;
+
+class Node (object):
+	def __init__ (self, label, weight, left, right):
+		self.label = label;
+		self.weight = weight;
+		self.left, self.right = left, right;
+
+	def __lt__ (self, other):
+		return (self.weight < other.weight);
+
+class HTree (object):
+	def __init__ (self):
+		self.root, self.nodes = None, set ();
+
+	def construct (self, frequencies):
+		nodes = [Node (label, frequencies [label], None, None) for label in frequencies] + [Node (None, float ('inf'), None, None)];
+		heapify (nodes);
+
+		x, y = heappop (nodes), heappop (nodes);
+		while (nodes):
+			parent = Node (None, x.weight + y.weight, x, y);
+			self.nodes = self.nodes.union (set ([x, y]));
+
+			heappush (nodes, parent);
+			x, y = heappop (nodes), heappop (nodes);
+
+		self.nodes.add (x);
+		self.root = x;
+
+	def get_encodings (self):
+		#needs to be implemented
+		if (not self.nodes):
+			return ([]);
+
+if (__name__ == '__main__'):
+	freq = {
+		'A' : 5,
+		'B' : 9,
+	    	'C' : 12,
+	    	'D' : 13,
+	    	'E' : 16,
+	    	'F' : 45
+	}
+	huff_tree = HTree ();
+
+	huff_tree.construct (freq);
+	encodings = huff_tree.get_encodings ();
+	for char in encodings: print ('%s : %d' % (char, encodings [char]));
